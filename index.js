945cd89d3d0ddd503a25c57ec200083269839d5c
HatouHarrisu <66234381+Harrisu-pm@users.noreply.github.com>
2022-06-18 13:51:47 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+require("dotenv").config();
+ 
+const client = new Discord.Client({
+    intents: 32767
+})
+const fs = require('fs')
+var figlet = require('figlet');
+
+fs.readdir('./commands/', (err, files) => {
+  if (err) return console.log('Could not find any commands!')
+  const jsFiles = files.filter(f => f.split('.').pop() === 'js')
+  if (jsFiles.length <= 0) return console.log('Could not find any commands!')
+  jsFiles.forEach(file => {
+    const cmd = require(`./commands/${file}`)
+    console.log(`Loaded ${file}`)
+    client.commands.set(cmd.name, cmd)
+    if (cmd.aliases) cmd.aliases.forEach(alias => client.aliases.set(alias, cmd.name))
+  })
+})
+client.on('ready', () => {
+  client.user.setActivity(`Hello world`, { type: "PLAYING" });
+  console.log("Logged in as")
+  figlet(client.user.tag, function(err, data) {
+    if (err) {
+        console.log('Error...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+  })
+});
+
+client.login(process.env.TOKEN)
